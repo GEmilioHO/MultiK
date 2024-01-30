cb8cfaae65c04289475fc15bf97132c5024c789a
Gabriel E Herrera-Oropeza <65782893+GEmilioHO@users.noreply.github.com>
2024-01-30 22:15:32 +0000

    Update MultiK.R

--- R/MultiK.R
+++ R/MultiK.R
@@ -85,6 +85,12 @@ MultiK <- function(seu, resolution = seq(0.05, 2, 0.05), nPC = 30, reps = 100, p
 
       # Set shared new HVG genes as Variable Features
       VariableFeatures(subX) <- shared.hvg
+
+      if (integrated.assay.norm.method == "LogNorm") {
+        # Scaling unwanted variation
+        all.genes <- rownames(x = subX)
+        subX <- ScaleData(object = subX, features = all.genes, vars.to.regress = vars.to.regress, verbose = F)
+      }
       
     }
 
